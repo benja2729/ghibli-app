af342fb86077411a0f74f59c88ce84d14a895d0f
Ben Fischer <bfischervu@gmail.com>
2019-05-29 13:34:46 -0500

    update styles and formatting

--- src/framework/helpers/Cache.js
+++ src/framework/helpers/Cache.js
@@ -1,16 +1,15 @@
-
 export default function Cache(getUnset) {
-	class CacheMap extends Map {
-		get(key) {
-			if (this.has(key)) {
-				return this.get(key);
-			}
+  class CacheMap extends Map {
+    get(key) {
+      if (this.has(key)) {
+        return this.get(key);
+      }
 
-			const value = getUnset(key);
-			this.set(key, value);
-			return value;
-		}
-	}
+      const value = getUnset(key);
+      this.set(key, value);
+      return value;
+    }
+  }
 
-	return new CacheMap;
+  return new CacheMap();
 }
--- src/framework/helpers/Mixin.js
+++ src/framework/helpers/Mixin.js
@@ -1,40 +1,42 @@
 import Cache from './Cache.js';
 
 const DESCRIPTOR_CACHE = Cache(MixinClass => {
-	let parent = MixinClass;
-	const protoDescs = {};
+  let parent = MixinClass;
+  const protoDescs = {};
 
-	while (parent !== Mixin) {
-		const parentDescs = Object.getOwnPropertyDescriptors(parent.prototype)
+  while (parent !== Mixin) {
+    const parentDescs = Object.getOwnPropertyDescriptors(parent.prototype);
 
-		for (const [key, desc] of Object.entries(parentDescs)) {
-			if (!protoDescs[key]) {
-				protoDescs[key] = desc;
-			}
-		}
+    for (const [key, desc] of Object.entries(parentDescs)) {
+      if (!protoDescs[key]) {
+        protoDescs[key] = desc;
+      }
+    }
 
-		parent = Object.getPrototypeOf(parent);
-	}
+    parent = Object.getPrototypeOf(parent);
+  }
 
-	delete protoDescs.constructor;
-	return protoDescs;
+  delete protoDescs.constructor;
+  return protoDescs;
 });
 
 export default class Mixin {
-	static apply(SuperClass) {
-		const { prototype: proto } = SuperClass;
-		const mixinDescs = DESCRIPTOR_CACHE.get(this);
+  static apply(SuperClass) {
+    const { prototype: proto } = SuperClass;
+    const mixinDescs = DESCRIPTOR_CACHE.get(this);
 
-		for (const [key, desc] of Object.entries(mixinDescs)) {
-			if (proto.hasOwnProperty(key)) {
-				throw new Error(`[Mixin] Cannot overwrite '${key}' from ${this} onto ${SuperClass}`);
-			} else {
-				Object.defineProperty(proto, key, desc);
-			}
-		}
-	}
+    for (const [key, desc] of Object.entries(mixinDescs)) {
+      if (proto.hasOwnProperty(key)) {
+        throw new Error(
+          `[Mixin] Cannot overwrite '${key}' from ${this} onto ${SuperClass}`
+        );
+      } else {
+        Object.defineProperty(proto, key, desc);
+      }
+    }
+  }
 
-	constructor() {
-		throw new Error('[Mixin] You cannot create an instance of a Mixin')
-	}
+  constructor() {
+    throw new Error('[Mixin] You cannot create an instance of a Mixin');
+  }
 }
